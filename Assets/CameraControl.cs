d0d095da5ac5e203186dc6ec9f2b688f8f16d0bd
dlghkdud <dlflo00f@gmail.com>
2024-06-03 15:07:18 +0900

    장면전환, 카메라 무빙 다 완성
    세부적인 이벤트, 대화창 만들어야됨 / 마켓 배경이랑 물건 배치해야됨 / 씬마다 어울리는 bgm찾아넣기

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target; // 캐릭터를 따라다닐 대상(타겟)의 Transform
+    public float minX; // 카메라가 이동할 수 있는 최소 x 위치
     public float maxX; // 카메라가 이동할 수 있는 최대 x 위치
 
     void Update()
@@ -12,8 +13,8 @@ public class CameraFollow : MonoBehaviour
             // 타겟의 현재 위치를 가져와서 카메라의 위치를 타겟의 x좌표에 맞추고 y와 z좌표는 유지
             float targetX = target.position.x;
 
-            // 카메라의 x 좌표가 maxX를 넘지 않도록 제한
-            float newX = Mathf.Min(targetX, maxX);
+            // 카메라의 x 좌표가 minX와 maxX 사이에 있도록 제한
+            float newX = Mathf.Clamp(targetX, minX, maxX);
 
             transform.position = new Vector3(newX, transform.position.y, transform.position.z);
         }
