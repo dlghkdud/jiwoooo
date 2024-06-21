f76341256b3a82dfd1954d6a84a39bb2bb331b7e
dlghkdud <dlflo00f@gmail.com>
2024-06-21 11:32:37 +0900

    슬롯

---
+++ Assets/ObjectItem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+public interface IObjectItem
+{
+    Item ClickItem();
+}
+
+public class ObjectItem : MonoBehaviour, IObjectItem
+{
+
+    [Header("아이템")]
+    public Item item;
+    [Header("아이템 이미지")]
+    public SpriteRenderer itemImage;
+
+    void Start()
+    {
+        itemImage.sprite = item.itemImage;
+    }
+    public Item ClickItem()
+    {
+        return this.item;
+    }
+}
